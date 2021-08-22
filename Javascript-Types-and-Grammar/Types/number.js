1f1c8229b20d3c1746778989eba244315e056d32
elhakimdev <abdulelhakim68@gmail.com>
2021-08-22 22:03:02 +0700

    javascript string - number literal

---
+++ Javascript-Types-and-Grammar/Types/number.js
@@ -0,0 +1,22 @@
+/**
+ * The number in javascrript
+ * 
+ * Jacascript provided number literal and number object
+ * let deeping it. 
+ * its base that beginner must know.
+ * to create number we can using one of these methods
+ * 
+ * number Literal
+ * number Object (Also call primitives).
+ */
+
+/**
+ * Javacscript number literal.
+ */
+let number = 0;
+
+/**
+ * Checking the type in javascript.
+ */
+typeof number; // return "number".
+typeof number === "number"; // will return true.
---
+++ Javascript-Types-and-Grammar/Types/string.js
@@ -0,0 +1,22 @@
+/**
+ * The string in javascrript
+ * 
+ * Jacascript provided string literal and string object
+ * let deeping it. 
+ * its base that beginner must know.
+ * to create string we can using one of these methods
+ * 
+ * String Literal
+ * String Object (Also call primitives).
+ */
+
+/**
+ * Javacscript string literal.
+ */
+let string = "The Javascript String Literal";
+
+/**
+ * Checking the type in javascript.
+ */
+typeof string; // return "string".
+typeof string === "string"; // will return true.
